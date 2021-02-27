771afff966739fe66589a8d6066faf111de76e1d
Jhon Gil <jccolombian@gmail.com>
2021-02-26 21:05:44 -0500

    primer commit: proyecto inicial newces2. Adding estilos y el index de la página

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE htlm>
+<html lang="es">
+    <head>
+        <title> My first page</title>
+        <meta charset="utf-8"
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link rel="stylesheet" type="text/css" href="css/normalize.css" />
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
+    </head>
+    <body>
+    <div id="easel">       
+        <header>
+        <h1>Programaci&oacute;n Web<h1>
+        </header>
+        <div id="main">
+            <p>
+            En este curso aprender&aacute;s a programar p&aacute;ginas web con git.
+            </p>
+        </div>
+        </div>
+    </body
+</html>    
